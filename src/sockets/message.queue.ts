9bf0a1ef7d2069ce75ec6e05295a2ff9ac2e39d7
Kiran Kharade <kiran.kharade@reanfoundation.org>
2024-01-27 19:04:39 +0530

    cleanup

--- src/sockets/message.queue.ts
+++ src/sockets/message.queue.ts
@@ -1,4 +1,3 @@
-// import PQueue from 'p-queue';
 import { ConversationDomainModel } from '../domain.types/chat/conversation.domain.model';
 import { ConversationDto } from '../domain.types/chat/conversation.dto';
 import { ChatMessageDomainModel } from "../domain.types/chat/chat.message.domain.model";
@@ -57,36 +56,6 @@ export class MessageQueue {
             }
         });
     };
-
-    // public static enqueueMessage(messageModel: ChatMessageDomainModel) {
-    //     (async () => {
-    //         await MessageQueue._messageQueue.add(async () => {
-    //             console.log(`Message enqueued: ${JSON.stringify(messageModel, null, 2)}`);
-    //             const chatService = Loader.container.resolve(ChatService);
-    //             const messageRecord: ChatMessageDto = await chatService.sendMessage(messageModel);
-    //             console.log(`Message sent: ${JSON.stringify(messageRecord, null, 2)}`);
-    //         });
-    //     })();
-    // }
-
-    // public static enqueueConversation(conversationModel: ConversationDomainModel) {
-    //     (async () => {
-    //         await MessageQueue._conversationQueue.add(async () => {
-    //             console.log(`Conversation enqueued: ${JSON.stringify(conversationModel, null, 2)}`);
-    //             const conversionId = conversationModel.id;
-    //             const chatService = Loader.container.resolve(ChatService);
-    //             if (conversionId) {
-    //                 const conversationRecord: ConversationDto = await chatService.getConversationById(conversionId);
-    //                 if (conversationRecord) {
-    //                     console.log(`Conversation already exists: ${JSON.stringify(conversationRecord, null, 2)}`);
-    //                     return;
-    //                 }
-    //             }
-    //             const conversationRecord: ConversationDto = await chatService.startConversation(conversationModel);
-    //             console.log(`Conversation started: ${JSON.stringify(conversationRecord, null, 2)}`);
-    //         });
-    //     })();
-    // }
 
     public static storeMessage(model: ChatMessageDomainModel) {
         try {
